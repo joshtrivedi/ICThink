6c4b34800ec0961ce088fa2290ac703f3c5d2ecd
Josh Trivedi <josh.trivedi@gmail.com>
2021-04-10 23:02:48 +0530

    purge training, new libraries import

--- training.py
+++ training.py
@@ -1,69 +1,9 @@
-import random
-from nltk import metrics
-from nltk.tag import sequential
-import numpy as np
-import json
-import pickle
-
-import nltk
-from nltk.stem import  WordNetLemmatizer
-from tensorflow.keras import optimizers
-import tensorflow.keras as keras
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
-
-lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json').read())
-
-count = 0
-words = []
-classes = []
-ignore_letters = ['?','!',',','.',"'"]
-documents = []
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        word_list = nltk.word_tokenize(pattern)
-        words.extend(word_list)
-        documents.append((word_list,intent['tag']))
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-
-words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
-words = sorted(set(words))
-
-classes = sorted(set(classes))
-
-pickle.dump(words, open('words.pkl','wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
-
-training = []
-output_empty =  [0]*len(classes) #np.zeros(len(classes))
-for document in documents:
-    bag = []
-    word_patterns = document[0]
-    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
-    for word in words:
-        if word in word_patterns:
-            bag.append(1) if word in word_patterns else bag.append(0)
-    output_row = list(output_empty)
-    output_row[classes.index(document[1])] = 1
-    training.append([bag,output_row])
-
-random.shuffle(training)
-training = np.array(training)
-train_x = list(training[:,0])
-train_y = list(training[:,1]) 
-
-model = keras.models.Sequential()
-
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
-
-gd = SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=gd, metrics=['accuracy'])
-model.fit(np.array(train_x), np.array(train_y),epochs=200,batch_size=5,verbose=1)
-model.save('chatbot-model.model')
-print('Done!')
+import json 
+import numpy as np 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
